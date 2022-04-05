9068ff7d57551ee6f17b16c58f946fde01bb5119
Miguel Morales <minesto23@gmail.com>
2022-04-05 08:58:17 -0400

    variables examples

--- C\C++/variables/main.c
+++ C\C++/variables/main.c
@@ -15,11 +15,13 @@ int main ()
 	char name[] = "bro"; //array of characters
 	bool e = true; //boolian true or false
 
+	const float PI = 3.1415; // constant
+
 	printf("Hello %s\n",name);
 	printf("You are %d years old\n",age);
 	printf("Your average grade is %c\n",grade);
 	printf("Your gpa is %.2f\n",gpa);
-
+	printf("I can print the ASCII code of a character\nfor example: character %c is %i\n",grade,grade);
 
 	return 0;
 
